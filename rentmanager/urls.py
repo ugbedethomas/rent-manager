58e0d46c0230eb435941268c35180deb0b09ac9b
Augustine Ugbede Thomas <factstheatre@gmail.com>
2025-07-05 00:00:42 +0100

    Add superuser creation route

--- rentmanager/urls.py
+++ rentmanager/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('', include('estate.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('run-migrations/', views.run_migrations, name='run_migrations'),  # ✅ Temporary migration URL             
-    path('create-admin/', views.create_superuser, name='create_admin'),
+    path('run-migrations/', views.run_migrations, name='run_migrations'),
+    path('create-admin/', views.create_superuser, name='create_superuser'),
 ]
+
 
